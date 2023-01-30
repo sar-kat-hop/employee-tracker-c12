03e24f277adf2b3f8c406d19f51249facd89af69
Sarah <sarah.k.hopper@gmail.com>
2023-01-29 20:27:22 -0800

    fix syntax in schema

--- public/db/schema.sql
+++ public/db/schema.sql
@@ -3,21 +3,22 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
-    id INT PRIMARY KEY
-    name VARCHAR(30)
-)
 CREATE TABLE employee (
-    id INT PRIMARY KEY
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    role_id INT
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
     manager_id INT
-)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+);
 
 CREATE TABLE role (
-    id INT PRIMARY KEY
-    title VARCHAR(30)
-    salary DECIMAL
-    department_id INT
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+);
