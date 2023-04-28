6b02530d6d523b8c82e9a6b197ae48d20b481afb
Sarah <sarah.k.hopper@gmail.com>
2023-04-27 17:59:36 -0700

    refactor schema to remove non-required fields, add "null" to first inserted employee to address column count mismatch error, successfully create db and tables using mysql shell

--- Dev/db/schema.sql
+++ Dev/db/schema.sql
@@ -7,13 +7,13 @@ USE company_db;
 -- mysql syntax errors when attempting to insert any table with an id field, but seems to accept 'X_id' without issue.
 CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (department_id)
 );
 
 CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
+    title VARCHAR(100),
     salary DECIMAL,
     department_id INT,
     -- department_name VARCHAR(30),
@@ -22,20 +22,7 @@ CREATE TABLE roles (
     -- FOREIGN KEY (department_name) REFERENCES departments(department_name) ON DELETE SET NULL
 );
 
-CREATE_TABLE employees (
-    ee_id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    department_id VARCHAR(30),
-    -- role_id INT,
-    manager_id INT,
-    -- title VARCHAR(30),
-    PRIMARY KEY (ee_id),
-    -- FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE SET NULL,
-    -- FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employees(ee_id) ON DELETE SET NULL,
-    -- FOREIGN KEY (title) REFERENCES roles(title) ON DELETE SET NULL
-);
+CREATE TABLE employees (ee_id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(50), last_name VARCHAR(50), manager_id INT, PRIMARY KEY (ee_id), FOREIGN KEY (manager_id) REFERENCES employees(ee_id) ON DELETE SET NULL);
 
 INSERT INTO departments (department_id, department_name)
     VALUES 
@@ -54,13 +41,13 @@ INSERT INTO roles (role_id, title, salary, department_id)
     ('7', 'Accupuncturist', '65000', '3'),
     ('8', 'Front Office Assistant', '30000', '2');
     
-INSERT INTO employees (ee_id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees (ee_id, first_name, last_name, manager_id)
     VALUES 
-    ('1', 'Ann', 'Marshall', '1', 'null'),
-    ('2', 'Karin', 'Chase', '2', '1'),
-    ('3', 'Caitlin', 'Henderson', '3', '1'),
-    ('4', 'Kory', 'Gilbert', '4', '3'),
-    ('5', 'Margo', 'Hawkins', '4', '4'),
-    ('6', 'Eli', 'Rosales', '6', '3'),
-    ('7', 'Chandra', 'Carter', '7', '3'),
-    ('8', 'Del', 'Savage', '8', '3');
+    ('1', 'Ann', 'Marshall', null),
+    ('2', 'Karin', 'Chase', '1'),
+    ('3', 'Caitlin', 'Henderson', '1'),
+    ('4', 'Kory', 'Gilbert', '3'),
+    ('5', 'Margo', 'Hawkins', '4'),
+    ('6', 'Eli', 'Rosales', '3'),
+    ('7', 'Chandra', 'Carter', '3'),
+    ('8', 'Del', 'Savage', '3');
